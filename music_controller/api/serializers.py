dec6d98fa59e39c8f93a70cf94255a9b0d9c6618
dustyAlgo <mrityunjaybhagat2002@gmail.com>
2025-10-20 01:32:01 +0530

    APIView02

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -1,1 +1,7 @@
-from rest_frameworks import serializers
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ("id", "code", "host", "guest_can_pause","VOtes_to_skip", "created_at")
--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 urlpatterns = [
-    path("main", main),
-    path('', main)
+    path("", RoomView.as_view()),
 ]
  
